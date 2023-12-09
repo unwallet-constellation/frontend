996f8d2eeb42417f6adced94ae13fa739541fe01
Dennis Zoma <dennis@scio.xyz>
2023-12-09 07:47:25 +0100

    chore: Update Next.js

--- src/app/setup/[[...stepId]]/page.tsx
+++ src/app/setup/[[...stepId]]/page.tsx
@@ -1,8 +1,11 @@
+import isEqual from 'lodash.isequal'
+
 import { ONBOARDING_STEPS } from './config'
+import { OnboardingStep } from './types'
 
 export default function OnboardingPage({ params }: { params: { stepId: [string] } }) {
   const { stepId } = params
-  const step = ONBOARDING_STEPS.find((s) => s.id === stepId[0])
+  const step: OnboardingStep | undefined = ONBOARDING_STEPS.find((s) => isEqual(stepId, [s.id]))
   if (!step) return null
 
   return <step.component {...{ step }} />
